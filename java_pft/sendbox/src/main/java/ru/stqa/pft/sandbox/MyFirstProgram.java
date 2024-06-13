642ffc93e0084dffcbab69482738e7f484d24157
Lenonkun <lenonkun10@gmail.com>
2024-06-13 08:38:48 +0300

    Выполнение задания блока 1

--- java_pft/sendbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ java_pft/sendbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,7 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("12341234");
+        System.out.println("проблемный грэдл");
+
     }
 }
---
+++ java_pft/sendbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,52 @@
+package ru.stqa.pft.sandbox;
+
+class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    //Для вычисления в методе
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    //Для вычисления в классе
+    public double distanceClass(Point p) {
+        double dX = this.x - p.x;
+        double dY = this.y - p.y;
+        return Math.sqrt(dX * dX + dY * dY);
+    }
+}
+
+class Run {
+    public static void main(String[] args) {
+
+        Point point1 = new Point(2, 6);
+        Point point2 = new Point(1, 7);
+
+        double d1 = distance(point1, point2);
+        System.out.println("Расстояние между точками в методе = " + d1);
+
+        double d2 = point1.distanceClass(point2);
+        System.out.println("Расстояние между точками в методе класса = " + d2);
+
+    }
+
+    public static double distance(Point p1, Point p2) {
+        double dX = p1.getX() - p2.getX();
+        double dY = p1.getY() - p2.getY();
+        return Math.sqrt(dX * dX + dY * dY);
+    }
+}
+
+
+//
+
