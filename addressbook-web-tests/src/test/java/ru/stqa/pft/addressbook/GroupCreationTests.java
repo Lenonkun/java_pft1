fbb6d6fcf19567b852dae3c5921a192ddd5f9419
Lenonkun <lenonkun10@gmail.com>
2024-06-14 14:02:58 +0300

    Добавление фикстуры по третьему заданию

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -17,10 +17,6 @@ public class GroupCreationTests {
     wd = new EdgeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     js = (JavascriptExecutor) wd;
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("http://localhost/addressbook/");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
@@ -29,17 +25,21 @@ public class GroupCreationTests {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys("group2");
+    wd.findElement(By.name("group_name")).sendKeys("group3");
     wd.findElement(By.name("group_header")).click();
     wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys("group2 header");
+    wd.findElement(By.name("group_header")).sendKeys("group3 header");
     wd.findElement(By.name("group_footer")).click();
     wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys("group2 footer");
+    wd.findElement(By.name("group_footer")).sendKeys("group3 footer");
     wd.findElement(By.name("submit")).click();
     wd.findElement(By.linkText("group page")).click();
     wd.findElement(By.linkText("Logout")).click();
